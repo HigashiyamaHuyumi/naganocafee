24f42cfc0843e63c236601286ef0a512af227985
HigasiyamaHuyumi <141430431+HigashiyamaHuyumi@users.noreply.github.com>
2023-10-16 18:33:31 +0000

    202310170333

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
         delete :destroy_all
       end
     end
-    resources :orders, only: [:new, :index, :show]
+    resources :orders, only: [:new, :create, :index, :show]
 	end
 	
 	namespace :admin do
@@ -39,5 +39,6 @@ Rails.application.routes.draw do
   get '/items', to: 'public/items#index', as: 'item_index' #商品一覧ページルート
   get '/items/:id', to: 'public/items#show', as: 'item_show' #商品ページルート
   get '/cart_items', to: 'public/cart_items#index', as: 'cart_items_index' #カートページルート
-  post '/orders/confirmation', to: 'public/orders#confirmation', as: 'orders_confirmation' #注文情報確認ページルート
+#注文情報確認ページルート
+  post '/orders/confirmation', to: 'public/orders#confirmation', as: 'orders_confirmation'
 end
