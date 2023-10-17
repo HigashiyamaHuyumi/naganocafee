5548567e17225340695672c8b6e051f5aa86862f
HigasiyamaHuyumi <141430431+HigashiyamaHuyumi@users.noreply.github.com>
2023-10-17 15:32:57 +0000

    202310180032

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,25 +4,32 @@ class Public::OrdersController < ApplicationController
     @order = Order.new
   end
 
-  def create #注文確定処理
-  end
-
   def confirmation
     @cart_items = current_customer.cart_items # カート内の商品を取得
     @合計金額 = 0 # 合計金額を初期化
     @cart_items.each do |cart_item|
       @合計金額 += cart_item.subtotal
     end
+    
     @order = Order.new(order_params)
     @order.customer_id = current_customer.id
-    if @order.shipping_address == "customer_address" #ご自身の住所を使用する場合
+    @order.postage = 800  # 固定の送料金額（800円）
+    @order.total_payment =  @order.postage + @合計金額
+    
+    if params[:order][:shipping_address] == "customer_address" #ご自身の住所を使用する場合
       @order.shipping_postal_code = current_customer.postal_code
       @order.shipping_address = current_customer.address
       @order.shipping_name = "#{current_customer.last_name} #{current_customer.first_name}"
     end
-    # 注文情報を表示するビューにデータを渡す
-    render 'confirmation' # 注文確認画面のビューを表示
-
+    
+    if @order.save  #注文情報を保存できた場合の処理（ここにリダイレクトなどを追加）
+      redirect_to create_path
+    else #注文情報を保存できなかった場合の処理（エラーメッセージ表示などを追加）
+      render 'confirmation', total_payment: @total_payment  # 合計金額をビューに渡す
+    end
+  end
+  
+  def create #注文確定処理
   end
 
   def complete #注文完了画面
