c578b4db47ad3541da07628a42006c1c4862d947
HigasiyamaHuyumi <141430431+HigashiyamaHuyumi@users.noreply.github.com>
2023-10-26 13:33:09 +0000

    202310232232

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,15 +6,15 @@ class Public::OrdersController < ApplicationController
 
   def confirmation #注文情報確認
     @cart_items = current_customer.cart_items # カート内の商品を取得
-    @合計金額 = 0 # 合計金額を初期化
+  @total_payment = 0 # 合計金額を初期化
     @cart_items.each do |cart_item|
-      @合計金額 += cart_item.subtotal
+      @total_payment += cart_item.subtotal
     end
 
     @order = Order.new(order_params)
     @order.customer_id = current_customer.id
     @order.postage = 800  # 固定の送料金額（800円）
-    @order.total_payment =  @order.postage + @合計金額
+    @order.total_payment =  @order.postage + @total_payment
 
     @address_type = params[:order][:address_type]
     if @address_type == "customer_address"
@@ -31,34 +31,19 @@ class Public::OrdersController < ApplicationController
   def create
     @order = Order.new(order_params)
     @order.customer_id = current_customer.id
-    @order.postage = 800  # 固定の送料金額（800円）
-
+    @order.postage = 800  # 固定の送料金額（800円)
+ 
+    if @order.save!
     @cart_items = current_customer.cart_items # カート内の商品を取得
-    @合計金額 = 0 # 合計金額を初期化
     @cart_items.each do |cart_item|
-      @合計金額 += cart_item.subtotal
+      order_detail = OrderDetail.new(order_id: @order.id)
+      order_detail.purchase_price= cart_item.item.price
+      order_detail.amount = cart_item.amount
+      order_detail.item_id = cart_item.item_id
+      order_detail.save!
     end
-    @order.total_payment =  @order.postage + @合計金額
-
-    @order.payment_method = params[:order][:payment_method]
-    if @order.payment_method == "credit_card"
-      @order.payment_method = 0
-    elsif @order.payment_method == "transfer"
-      @order.payment_method = 1
-    end
-
-    @address_type = params[:order][:address_type]
-    if @address_type == "customer_address"
-      @order.shipping_postal_code = current_customer.postal_code
-      @order.shipping_address = current_customer.address
-      @order.shipping_name = current_customer.last_name + current_customer.first_name
-    elsif @address_type == "shipping_address"
-      @order.shipping_postal_code = params[:order][:shipping_postal_code]
-      @order.shipping_address = params[:order][:shipping_address]
-      @order.shipping_name = params[:order][:shipping_name]
-    end
-
-    if @order.save
+    
+      
       @cart_items.destroy_all #注文が正常に保存された場合、カートを空にする
       redirect_to orders_complete_path
     else
@@ -71,9 +56,12 @@ class Public::OrdersController < ApplicationController
   end
 
   def index #注文履歴画面
+    @orders = current_customer.orders.all
   end
 
-  def show　#注文履歴詳細画面
+  def show #注文履歴詳細画面
+    @order = Order.find(params[:id]) #注文IDから注文を取得
+    @order_details = @order.order_details.all # 注文に関連する商品情報を取得
   end
 
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,6 @@ class Item < ApplicationRecord
 
   has_one_attached :image
   has_many :cart_items
-  has_many :orders
   has_many :order_details
 
   def get_image
